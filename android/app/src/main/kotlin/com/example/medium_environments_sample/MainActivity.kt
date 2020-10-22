c0f259458b2e67a47f996fa7b08d988d7fbd8c0f
Bertuğ <bertug.yilmaz@d-teknoloji.com.tr>
2020-10-22 17:20:22 +0300

    init project

---
+++ android/app/src/main/kotlin/com/example/medium_environments_sample/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.medium_environments_sample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_config/flutter_config.dart';
+
+void main() {
+  FlutterConfig.loadEnvVariables();
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  const App({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      body: Container(
+          child: Center(
+        child: Text("Hellöğğ"),
+      )),
+    ));
+  }
+}
