1d5d8968d7f5307d7b93fe0d3b890ac5f20314f4
Bertuğ <bertug.yilmaz@d-teknoloji.com.tr>
2020-11-11 21:56:06 +0300

    Added firebase initialization code lines

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_config/flutter_config.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -8,15 +9,70 @@ void main() async {
 }
 
 class App extends StatelessWidget {
-  const App({Key key}) : super(key: key);
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  App({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Container(
-          child: Center(
-            child: Text("Hellöğğ"),
+        home: FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return ErrorPage();
+        }
+
+        if (snapshot.connectionState == ConnectionState.done) {
+          return HomePage();
+        }
+
+        return LoadingView();
+      },
+    ));
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: Text("Hellöğğ"),
+        ),
+      ),
+    );
+  }
+}
+
+class LoadingView extends StatelessWidget {
+  const LoadingView({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: CircularProgressIndicator(),
+        ),
+      ),
+    );
+  }
+}
+
+class ErrorPage extends StatelessWidget {
+  const ErrorPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: Text(
+            "Something went wrong...",
           ),
         ),
       ),
